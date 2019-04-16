38b0f1a7f4d1d8f19aa7b71d947d30aa199d3031
Dávid Szabó <davidka1997@gmail.com>
2019-04-16 21:40:49 +0200

    HttpClientModule imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import {HomepageComponent} from './components/frame/homepage/homepage.component'
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './services/app-routing.module';
+import { MembersListComponent } from './components/frame/list/members-list/members-list.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    MembersListComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     RouterModule
   ],
   providers: [],
