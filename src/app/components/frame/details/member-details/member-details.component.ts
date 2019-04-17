676ca961d67ad78fa8698ad0350ec1dd3022c2fd
Dávid Szabó <davidka1997@gmail.com>
2019-04-17 17:04:23 +0200

    member-details added

---
+++ src/app/components/frame/details/member-details/member-details.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {MemberService} from '../../../../services/member.service';
+import {ActivatedRoute} from '@angular/router';
+import {Member} from '../../../../models/member';
+
+@Component({
+  selector: 'app-member-details',
+  templateUrl: './member-details.component.html',
+  styleUrls: ['./member-details.component.css']
+})
+export class MemberDetailsComponent implements OnInit {
+
+  member: Member;
+
+  constructor(
+    private memberService: MemberService,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit() {
+    this.getMember();
+  }
+
+  getMember(): void {
+    this.memberService.getMember(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe((member: Member) => this.member = member);
+  }
+
+}
